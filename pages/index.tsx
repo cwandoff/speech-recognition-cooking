cd5fa164618bf6d47e5f0feaf83c2d77d07b9c9a
FaultyPaladin <cwandoff@gmail.com>
2023-03-22 14:02:37 -0400

    basic commands and skeleton methods that will be used to handle them

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,77 +1,109 @@
 import * as React from "react";
-import { TestButton } from "../src/components/ButtonTest";
-// import {Dictaphone } from "../src/components/Dictaphone"
 import { Content } from "../src/components/ui/Content";
-import Button from "@mui/material/Button";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import { useState } from "react";
-// import "regenerator-runtime/runtime";
+
+
+interface Recipe{
+  title: string;
+  ingredients: string[];
+  instructions: string;
+  picture_link: string;
+}
 
 const HomePage = (_props: any) => {
   const [message, setMessage] = useState("");
+  const [currentTranscript, setCurrentTranscript] = useState("");
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [currentInstruction, setCurrentInstruction] = useState("");
+
+  const handleKeywords = (keyword: string) => {
+
+    setCurrentTranscript(transcript)
+    console.log(currentTranscript)
+    resetTranscript()
+    return keyword as string;
+  }
+
+  const handleFilter = (recipeType: string) => {
+   
+    return recipeType as string;
+  }
+
+  const handleStart = (recipeName: string) => {
+
+    /**
+     * NEED JSON IMPLEMENTATION FIRST
+     * USE MUI ELEMENTS TO DISPLAY STEPS + SPEECH OUTPUT
+     */
+
+    // setCurrentRecipe(recipeName)
+
+  }
+
+  const handleNext = () => {
+    /** 
+     * GO TO NEXT STEP IN THE RECIPE
+     */
+  }
+
+  const handleEnd = () => {
+    setCurrentRecipe(null);
+    //POTENTIAL AUDIO OUTPUT
+  }
+
+  const recipeExists = (recipeName: string) => {
+    /**
+     * FIGURE OUT HOW TO PARSE THROUGH THE JSON
+     */
+  }
+
+  const getRecipe = (recipeName: string) => {
+    /**
+     * SEARCH THROUGH RECIPE LIST
+     *
+     * IF THE RECIPE AT WHATEVER INDEX HAS THE SAME TITLE
+     *  RETURN THAT RECIPE AS A RECIPE TYPE
+     */
+  }
+
   const commands = [
     {
-      command: ["Hello", "Hi"],
-      callback: ({ command }) => setMessage(`Hi there! You said: "${command}"`),
-      matchInterim: true,
+      command: 'Show me a :recipeType recipe',
+      callback: (recipeType: any) => setMessage(`Hey, showing a ${handleFilter(recipeType)} recipe`)
     },
     {
-      command: ["Test"],
-      callback: () => setMessage("This is a good test message!"),
+      command: 'Start (the) :recipe recipe',
+      callback: (recipe: any) => setMessage(`Starting recipe: ${handleKeywords(recipe)} ${handleStart(recipe)}`)
     },
     {
-      command: "clear",
-      callback: ({ resetTranscript }) => resetTranscript(),
+      command: 'Next (step)',
+      callback: () => setMessage(`Now showing ${handleNext()}`)
+    },
+    {
+      command: 'End (recipe)',
+      callback: () => setMessage(`Ending recipe ${handleEnd()}`)
+    },
+    {
+      command: 'reset',
+      callback: () => resetTranscript()
     },
   ];
 
-  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition(
-    { commands }
-  );
-
-  // if (!browserSupportsSpeechRecognition) {
-  //   return <span>Browser doesn't support speech recognition.</span>;
-  // }
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
 
   SpeechRecognition.startListening({ continuous: true });
 
   return (
     <Content>
-      {/* <Button onClick={SpeechRecognition.startListening}>Start</Button>
-      <Button onClick={SpeechRecognition.stopListening}>Stop</Button> */}
 
       <p>{message}</p>
-      <p>{transcript}</p>
+      <p>{currentTranscript}</p>
+
     </Content>
   );
 };
 
 export default HomePage;
-
-// import React from 'react';
-
-// const Dictaphone = () => {
-//   const {
-//     transcript,
-//     listening,
-//     resetTranscript,
-//     browserSupportsSpeechRecognition
-//   } = useSpeechRecognition();
-
-//   if (!browserSupportsSpeechRecognition) {
-//     return <span>Browser doesn't support speech recognition.</span>;
-//   }
-
-//   return (
-//     <div>
-//       <p>Microphone: {listening ? 'on' : 'off'}</p>
-//       <button onClick={SpeechRecognition.startListening}>Start</button>
-//       <button onClick={SpeechRecognition.stopListening}>Stop</button>
-//       <button onClick={resetTranscript}>Reset</button>
-//       <p>{transcript}</p>
-//     </div>
-//   );
-// };
-// export default Dictaphone;
