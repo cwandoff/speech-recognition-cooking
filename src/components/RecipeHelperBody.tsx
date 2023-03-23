5e7aac8a22db6ae1862f0b543639688c3650bfa6
FaultyPaladin <cwandoff@gmail.com>
2023-03-22 22:03:00 -0400

    JSON data parsed and skeleton handler methods have definitions

--- src/components/RecipeHelperBody.tsx
+++ src/components/RecipeHelperBody.tsx
@@ -1,123 +1,156 @@
 import * as React from "react";
 import SpeechRecognition, {
-  useSpeechRecognition,
+    useSpeechRecognition,
 } from "react-speech-recognition";
 import { useState } from "react";
 
+import * as recipesData from "./prototype files/recipes_raw/recipes_raw_nosource_ar.json"
 
-interface Recipe{
-  title: string;
-  ingredients: string[];
-  instructions: string;
-  picture_link: string;
+
+interface Recipe {
+    title: string;
+    ingredients: string[];
+    instructions: string[];
+    picture_link: string;
+}
+
+const recipes = Object.values(recipesData).map((val) => {
+    
+    return ({
+        title: val.title,
+        ingredients: val.ingredients,
+        instructions: val.instructions,
+        picture_link: val.picture_link
+    } as Recipe)
+
+})
+
+
+for(const r of recipes){
+    console.log(r.instructions[0]);
 }
 
 const RecipeHelperBody = (_props: any) => {
-  const [message, setMessage] = useState("");
-  const [currentTranscript, setCurrentTranscript] = useState("");
-  const [currentRecipe, setCurrentRecipe] = useState(null);
-  const [currentInstruction, setCurrentInstruction] = useState("");
+    const [message, setMessage] = useState("");
+    const [currentTranscript, setCurrentTranscript] = useState("");
+    const [currentRecipe, setCurrentRecipe] = useState<Recipe | null>(null);
+    const [filtered, setFiltered] = useState<Recipe[]>([]);
+    const [currentInstruction, setCurrentInstruction] = useState("");
 
-  const handleKeywords = (keyword: string) => {
 
-    setCurrentTranscript(transcript)
-    console.log(currentTranscript)
-    resetTranscript()
-    return keyword as string;
-  }
+    const handleFilter = (recipeType: string) => {
 
-  const handleFilter = (recipeType: string) => {
+        setMessage(`Now showing a ${recipeType} recipe`);
 
-    setMessage(`Now showing a ${recipeType} recipe`);
-   
-    return recipeType as string;
-  }
+        const searched = []
+        for (const r of recipes) {
+            if (r.title != undefined) {
+                if (r.title.includes(recipeType)) {
+                    searched.push(r);
+                }
+            }
+        }
 
-  const handleStart = (recipeName: string) => {
+        setFiltered(searched);
+    }
 
-    setMessage(`Starting recipe: ${handleKeywords(recipeName)}`);
-    /**
-     * NEED JSON IMPLEMENTATION FIRST
-     * USE MUI ELEMENTS TO DISPLAY STEPS + SPEECH OUTPUT
-     */
+    const handleStart = (recipeName: string) => {
 
-    // setCurrentRecipe(recipeName)
+        if (recipeExists(recipeName)) {
+            setCurrentRecipe(getRecipe(recipeName));
+            setMessage(`Starting recipe: ${currentRecipe?.title}`);
+        }
 
-  }
+    }
 
-  const handleNext = () => {
+    const handleNext = () => {
 
-    if(currentRecipe == null){
-        setMessage("No recipe has been started yet");
-    } else {
-        setMessage("Now showing the next step")
-        /** 
-         * GO TO NEXT STEP IN THE RECIPE
+        if (currentRecipe == null) {
+            setMessage("No recipe has been started yet");
+        } else {
+            setMessage("Now showing the next step")
+            /** 
+             * GO TO NEXT STEP IN THE RECIPE
+             */
+        }
+    }
+
+    const handleEnd = () => {
+        if (currentRecipe == null) {
+            setMessage("No recipe has been started yet");
+        } else {
+            setMessage("Ending recipe")
+            setCurrentRecipe(null);
+            //POTENTIAL AUDIO OUTPUT
+        }
+
+    }
+
+    const recipeExists = (recipeName: string) => {
+
+        for (const r of recipes) {
+            if (r.title != undefined) {
+                if (r.title == recipeName) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    const getRecipe = (recipeName: string) => {
+        /**
+         * SEARCH THROUGH RECIPE LIST
+         *
+         * IF THE RECIPE AT WHATEVER INDEX HAS THE SAME TITLE
+         *  RETURN THAT RECIPE AS A RECIPE TYPE
          */
+        for (const r of recipes) {
+            if (r.title != undefined) {
+                if (r.title == recipeName) {
+                    return r;
+                }
+            }
+        }
+        return null
     }
-  }
 
-  const handleEnd = () => {
-    if(currentRecipe == null){
-        setMessage("No recipe has been started yet");
-    } else {
-        setMessage("Ending recipe")
-        setCurrentRecipe(null);
-    //POTENTIAL AUDIO OUTPUT
-    }
-    
-  }
+    const commands = [
+        {
+            command: 'Show me a :recipeType recipe',
+            callback: (recipeType: any) => handleFilter(recipeType)
+        },
+        {
+            command: 'Start (the) :recipe recipe',
+            callback: (recipe: any) => handleStart(recipe)
+        },
+        {
+            command: 'Next (step)',
+            callback: () => handleNext()
+        },
+        {
+            command: 'End (recipe)',
+            callback: () => handleEnd()
+        },
+        {
+            command: 'reset',
+            callback: () => resetTranscript()
+        },
+    ];
 
-  const recipeExists = (recipeName: string) => {
-    /**
-     * FIGURE OUT HOW TO PARSE THROUGH THE JSON
-     */
-  }
+    const { transcript, resetTranscript } = useSpeechRecognition({ commands });
 
-  const getRecipe = (recipeName: string) => {
-    /**
-     * SEARCH THROUGH RECIPE LIST
-     *
-     * IF THE RECIPE AT WHATEVER INDEX HAS THE SAME TITLE
-     *  RETURN THAT RECIPE AS A RECIPE TYPE
-     */
-  }
+    SpeechRecognition.startListening({ continuous: true });
 
-  const commands = [
-    {
-      command: 'Show me a :recipeType recipe',
-      callback: (recipeType: any) => handleFilter(recipeType)
-    },
-    {
-      command: 'Start (the) :recipe recipe',
-      callback: (recipe: any) => handleStart(recipe)
-    },
-    {
-      command: 'Next (step)',
-      callback: () => handleNext()
-    },
-    {
-      command: 'End (recipe)',
-      callback: () => handleEnd()
-    },
-    {
-      command: 'reset',
-      callback: () => resetTranscript()
-    },
-  ];
+    return (
+        <div>
 
-  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+            <p>{message}</p>
+            <p>{currentTranscript}</p>
 
-  SpeechRecognition.startListening({ continuous: true });
-
-  return (
-    <div>
-
-      <p>{message}</p>
-      <p>{currentTranscript}</p>
-
-    </div>
-  );
+        </div>
+    );
 };
 
 export default RecipeHelperBody;
